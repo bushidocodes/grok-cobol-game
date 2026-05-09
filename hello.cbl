307ddebb9feb3c11c7f0d993c6ede8beed519588
Sean McBride <spmcbride1201@gmail.com>
2026-05-09 10:46:56 -0400

    Replace angry mob ASCII with molotov cocktail and burning Tesla art in GAME-OVER-SCREEN.

--- hello.cbl
+++ hello.cbl
@@ -55,12 +55,15 @@ SCREEN SECTION.
    05 LINE 4 COL 15 VALUE "==========================================".
    05 LINE 6 COL 25 VALUE "  GAME OVER  " HIGHLIGHT.
    05 LINE 8 COL 15 VALUE "   ANGRY MOB RIOTS!   " HIGHLIGHT.
-   05 LINE 10 COL 18 VALUE "   o     o     o     o   ".
-   05 LINE 11 COL 18 VALUE "  /|\   /|\   /|\   /|\  ".
-   05 LINE 12 COL 18 VALUE "  / \   / \   / \   / \  ".
-   05 LINE 14 COL 10 VALUE "Your approval hit ZERO. The citizens revolt!".
-   05 LINE 16 COL 15 VALUE "==========================================".
-   05 LINE 19 COL 20 VALUE "Press ENTER to exit...".
+   05 LINE 10 COL 12 VALUE "   Molotov      Burning Tesla".
+   05 LINE 11 COL 12 VALUE "     /\\          ___     ".
+   05 LINE 12 COL 12 VALUE "    /  \\        /   \\   ".
+   05 LINE 13 COL 12 VALUE "   |    |      |  o  |  ".
+   05 LINE 14 COL 12 VALUE "    \\  /        \\___/   ".
+   05 LINE 15 COL 12 VALUE "     \\/          fire!   ".
+   05 LINE 17 COL 10 VALUE "Your approval hit ZERO. The citizens revolt!".
+   05 LINE 19 COL 15 VALUE "==========================================".
+   05 LINE 21 COL 20 VALUE "Press ENTER to exit...".
 
 PROCEDURE DIVISION.
 MAIN-LOGIC.
